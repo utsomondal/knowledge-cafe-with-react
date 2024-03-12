25be0ac5e7577a1e72bbd5946f63e1bde8f18981
utsomondal <utsomondal2019@gmail.com>
2024-03-13 00:29:26 +0600

    some componnent & fake data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Blogs from './components/blogs/Blogs'
+import Header from './components/header/Header'
 
 function App() {
 
   return (
     <>
-      
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header>
+            <h1 className="text-6xl">Knowledge Cafe</h1>
+            <img src="" alt="user avatar" />
+        </header>
+    );
+};
+
+export default Header;
